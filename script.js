e909e40dd6d543f5d40e2757172d096f85b2105f
Oswaldo <rodriosw13@gmail.com>
2025-12-18 17:04:19 -0400

    Finishing the project tab extension

--- script.js
+++ script.js
@@ -4,63 +4,107 @@ const endpoint = 'photos/random'
 
 const unsplashURL = `${baseURL}${endpoint}?client_id=${unsplashApiKey}&orientation=landscape&query=nature`
 
-fetch(unsplashURL)
-  .then((response) => {
+async function fetchBackgroundImage() {
+  try {
+    const response = await fetch(unsplashURL)
+
     if (!response.ok) {
-      throw Error('Error fetching background Image')
+      throw Error(
+        `Error fetching background Image. ${response.status} ${response.statusText}`
+      )
     }
-    return response.json()
-  })
-  .then((data) => {
-    console.log(data.urls.regular)
-    // document.querySelector('#main').style.background = `url("${data.urls.regular}")`
-  })
-  .catch((error) => console.log(error))
+
+    const data = await response.json()
+    renderBackgroundImage(data)
+  } catch (err) {
+    console.error('Error fetching background Image', err)
+  }
+}
+
+function renderBackgroundImage(data) {
+  document.querySelector(
+    'body'
+  ).style.backgroundImage = `url(${data.urls.full})`
+  document.querySelector('#author').textContent = `Photo by: ${data.user.name}`
+}
+
+fetchBackgroundImage()
+
+//Currency
 
 const urlCoinGecko = 'https://api.coingecko.com/api/v3/coins/bitcoin'
 
-fetch(urlCoinGecko)
-  .then((response) => {
+async function fetchCurrency() {
+  try {
+    const response = await fetch(urlCoinGecko)
+
     if (!response.ok) {
-      throw Error('Error fetching bitcoin information')
+      throw Error(`${response.status} ${response.statusText}`)
     }
-    return response.json()
-  })
-  .then((data) => {
-    console.log(data)
-    document.querySelector('.crypto').innerHTML = `
-        <div class="crypto-head">
-            <img src="${data.image.small}" alt="Image of ${data.name}">
-            <p id="crypto-title" class="crypto-title">${data.name}</p>
-        </div>
-        <div class="crypto-information">
-            <p>🎯:<span>$${data.market_data.current_price.usd}</span></p>
-            <p>👆:<span>$${data.market_data.high_24h.usd}</span></p>
-            <p>👇:<span>$${data.market_data.low_24h.usd}</span></p>
-        </div>
+
+    const data = await response.json()
+
+    renderCurrency(data)
+  } catch (err) {
+    console.error(`Error fetching currency`, err)
+  }
+}
+
+function renderCurrency(data) {
+  document.querySelector('.crypto').innerHTML = `
+         <div class="crypto-head">
+           <img src="${data.image.small}" alt="Image of ${data.name}">
+             <p id="crypto-title" class="crypto-title">${data.name}</p>
+         </div>
+         <div class="crypto-information">
+             <p>🎯:<span>$${data.market_data.current_price.usd}</span></p>
+             <p>👆:<span>$${data.market_data.high_24h.usd}</span></p>
+             <p>👇:<span>$${data.market_data.low_24h.usd}</span></p>
+         </div>
     `
-  })
-  .catch((error) => console.log(error))
+}
+
+fetchCurrency()
+//Time and Date
 
 function getCurrentTime() {
   const date = new Date()
-  document.getElementById('time').textContent = `${date.toLocaleTimeString('en-US')}`
+  document.getElementById('time').textContent = `${date.toLocaleTimeString(
+    'en-US'
+  )}`
+  document.getElementById('date').textContent = `${date.toLocaleDateString()}`
 }
 
 setInterval(getCurrentTime, 1000)
 
-navigator.geolocation.getCurrentPosition((position) => {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=c67a42588def43b47f205d2a72dd6868&units=imperial`,
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw Error('Error fetching the weather conditions')
-      }
-      return response.json()
-    })
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((err) => console.log(err))
+async function fetchWeatherConditions(position) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=c67a42588def43b47f205d2a72dd6868&units=metric`
+    )
+
+    if (!response.ok) {
+      throw Error(`${response.status} ${response.statusText}`)
+    }
+
+    const data = await response.json()
+    renderWeather(data)
+  } catch (err) {
+    console.error(`Error fetching weather`, err)
+  }
+}
+
+function renderWeather(data) {
+  const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+  document.querySelector('.weather').innerHTML = `
+         <img src="${iconURL}" alt="Icon of ${data.weather[0].description}">
+        <p class="temperature">${data.main.temp.toFixed(1)}°C</p>
+        <p class="city">${data.name}</p>
+    
+    `
+}
+
+navigator.geolocation.getCurrentPosition(async (position) => {
+  await fetchWeatherConditions(position)
 })
